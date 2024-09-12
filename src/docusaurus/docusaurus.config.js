48fe0910b0ee519a94e5775904c89af56870b510
Nanditha K <106753017+nanditha938@users.noreply.github.com>
2024-09-12 21:45:50 +0530

    Created placeholders for elevate components
    Added Projects documentation updates

--- src/docusaurus/docusaurus.config.js
+++ src/docusaurus/docusaurus.config.js
@@ -94,6 +94,33 @@ const config = {
     [
       '@docusaurus/plugin-content-docs',
       {
+        id: 'projects',
+        path: 'projects',
+        routeBasePath: 'projects',
+        sidebarPath: require.resolve('./sidebarsProjects.js'),
+      },
+    ],
+    [
+      '@docusaurus/plugin-content-docs',
+      {
+        id: 'observationsorsurveys',
+        path: 'observationsorsurveys',
+        routeBasePath: 'observationsorsurveys',
+        sidebarPath: require.resolve('./sidebarsObservationsSurveys.js'),
+      },
+    ],
+    [
+      '@docusaurus/plugin-content-docs',
+      {
+        id: 'reports',
+        path: 'reports',
+        routeBasePath: 'reports',
+        sidebarPath: require.resolve('./sidebarsReports.js'),
+      },
+    ],
+    [
+      '@docusaurus/plugin-content-docs',
+      {
         id: 'userservice',
         path: 'userservice',
         routeBasePath: 'userservice',
@@ -139,6 +166,15 @@ const config = {
         },
       },
     ],
+    [
+      '@docusaurus/plugin-content-docs',
+      {
+        id: 'entitymanagementservice',
+        path: 'entitymanagementservice',
+        routeBasePath: 'entitymanagementservice',
+        sidebarPath: require.resolve('./sidebarsEntityManagement.js'),
+      },
+    ],
   ],
 
   themeConfig:
@@ -174,25 +210,56 @@ const config = {
         },
         {
           type: 'doc',
-          docId: 'settingup-userservice',
-          docsPluginId: 'userservice',
+          docId: 'intro',
+          docsPluginId: 'projects',
           position: 'left',
-          label: 'User Service',
+          label: 'Projects',
         },
         {
           type: 'doc',
-          docId: 'settingup-notificationservice',
-          docsPluginId: 'notificationservice',
+          docId: 'intro',
+          docsPluginId: 'observationsorsurveys',
           position: 'left',
-          label: 'Notification Service',
+          label: 'Observations/Surveys',
         },
         {
           type: 'doc',
-          docId: 'settingup-schedulerservice',
-          docsPluginId: 'schedulerservice',
+          docId: 'intro',
+          docsPluginId: 'reports',
           position: 'left',
-          label: 'Scheduler Service',
-        }, 
+          label: 'Reports',
+        },
+        {
+          type: 'dropdown',
+          label: 'Supporting Services',
+          position: 'left',
+          items: [
+            {
+              type: 'doc',
+              docId: 'settingup-userservice',
+              docsPluginId: 'userservice',
+              label: 'User Service',
+            },
+            {
+              type: 'doc',
+              docId: 'settingup-notificationservice',
+              docsPluginId: 'notificationservice',
+              label: 'Notification Service',
+            },
+            {
+              type: 'doc',
+              docId: 'settingup-schedulerservice',
+              docsPluginId: 'schedulerservice',
+              label: 'Scheduler Service',
+            },
+            {
+              type: 'doc',
+              docId: 'setup-entitymanagementservice',
+              docsPluginId: 'entitymanagementservice',
+              label: 'Entity Management Service',
+            },
+          ],
+        },         
         {
           href: 'https://elevate.shikshalokam.org/contact-us/',
           label: 'Help',
