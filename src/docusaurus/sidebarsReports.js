c692408f88d9473bfef074524d8d6442a08f86ce
ushajohn <usha.john95@gmail.com>
2025-02-03 17:39:30 +0530

    Added topics for reports documentation
    added topics for reports

--- src/docusaurus/sidebarsReports.js
+++ src/docusaurus/sidebarsReports.js
@@ -68,6 +68,125 @@ const sidebars = {
             },  
           ]   
         }, 
+        {
+          type: 'category',
+          label: 'Administrator Dashboard',
+          items:
+           [
+
+            {
+              type: 'doc',
+              id: 'aboutadmindashboard',
+              label: 'About Admin Dashboard',
+            },
+            
+            {type: 'doc',
+              id: 'adimprovementprojectreport', // document ID
+              label: 'Improvement Project Report', // sidebar label
+              },
+
+              {
+                type: 'doc',
+                id: 'adimprovementconsumptionreport', // document ID
+                label: 'Improvement Consumption Report', // sidebar label
+              },
+
+            {type: 'doc',
+              id: 'aduniqueuserimprovement', // document ID
+              label: 'Unique User Improvement Project Report', // sidebar label
+              }, 
+                     
+            ]
+          },
+          {
+            type: 'category',
+            label: 'Program Dashboard',
+            items:
+             [
+
+              {
+                type: 'doc',
+                id: 'aboutprogramdashboard',
+                label: 'About Program Dashboard',
+              },
+              
+                {
+                  type: 'doc',
+                  id: 'pmstatusreport', // document ID
+                  label: 'Status Report', // sidebar label
+                },
+                {
+                  type: 'doc',
+                  id: 'pmtaskreport', // document ID
+                  label: 'Task Report', // sidebar label
+                },
+                {
+                  type: 'doc',
+                  id: 'pmimpprojectstatusoverview', // document ID
+                  label: 'Improvement Projects Status Overview', // sidebar label
+                },
+              ]
+            },
+               {
+                type: 'category',
+                label: 'State Dashboard',
+                items:
+                [
+    
+                {
+                      type: 'doc',
+                      id: 'aboutstatedashboard',                                          
+                      label: 'About State Dashboard',
+                    },
+                    {type: 'doc',
+                      id: 'stconsumption', // document ID
+                      label: 'Improvement Consumption Overview', // sidebar label
+                      },
+                      {
+                        type: 'doc',
+                        id: 'stimprovement', // document ID
+                       
+                        label: 'Improvement Project Report [Karnataka]', // sidebar label
+                      }, 
+                      {
+                        type: 'doc',
+                        id: 'stuniqueuser', // document ID
+                        label: 'Unique User Improvement', // sidebar label
+                      }, 
+                    ]
+                  },
+                  {
+                    type: 'category',
+                    label: 'District Dashboard',
+                    items:
+                     [
+      
+                      {
+                        type: 'doc',
+                        id: 'aboutdistrictdashboard',
+                        label: 'About District Dashboard',
+                      }, 
+                      
+                      {
+                        type: 'doc',
+                        id: 'dtimprovementreport',
+                        label: 'Improvement Project Report',
+                        },
+                        {
+                          type: 'doc',
+                          id: 'dtimpconsumption',
+                          label: 'Improvement Consumption Report',
+                          },
+
+                          {
+                            type: 'doc',
+                            id: 'dtuniqueuser',
+                            label: 'Unique User Improvement Project Report',
+                            },          
+                         
+                
+              ]
+            },                  
 
 
          ],
